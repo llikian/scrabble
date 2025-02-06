932475a7cae5e329d56c24f7971e7f52e0da27fa
p2102474 <samuel.bibas@etu.univ-lyon1.fr>
2025-02-06 08:54:05 +0100

    Type declarations for dictionary structure

---
+++ include/Dictionary.hpp
@@ -0,0 +1,35 @@
+﻿/***************************************************************************************************
+* @file  Dictionary.hpp
+ * @brief Declaration of the dictionary class
+ **************************************************************************************************/
+
+#ifndef DICTIONARY_H
+#define DICTIONARY_H
+
+#include <string>
+#include <vector>
+
+struct Node
+{
+    char value;
+    bool isTerminal;
+    std::vector<Node*> children;
+
+    Node(char value, bool isTerminal);
+};
+
+//Trie structure
+class Dictionary {
+private:
+    Node* root;
+
+public:
+    Dictionary();
+
+    void insertWord(const std::string& word);
+    bool containWord(const std::string& word);
+};
+
+
+
+#endif //DICTIONARY_H
---
+++ src/Dictionary.cpp
@@ -0,0 +1,26 @@
+﻿//
+// Created by Foyeur on 06/02/2025.
+//
+
+#include "Dictionary.hpp"
+
+#include <algorithm>
+
+Node::Node(const char value, const bool isTerminal) : value(value), isTerminal(isTerminal){ }
+
+Dictionary::Dictionary()
+{
+    root = new Node('\0', false);
+}
+
+void Dictionary::insertWord(const std::string& word)
+{
+
+}
+
+
+bool Dictionary::containWord(const std::string& word)
+{
+    return false;
+}
+
