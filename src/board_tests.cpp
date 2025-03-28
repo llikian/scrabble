c5fcdd7e971b18773ddf76f85727753a4d6f507f
llikian <pokillian64@gmail.com>
2025-03-28 19:45:50 +0100

    More tests

--- src/board_tests.cpp
+++ src/board_tests.cpp
@@ -12,17 +12,23 @@
 #include "Player.hpp"
 
 int main() {
-    Dictionary dictionary("data/dico.txt");;
-    Bag bag;
-    Player player(bag);
-    Board board(bag, dictionary);
+    for(unsigned int boards = 0 ; boards < 100 ; ++boards) {
+        Dictionary dictionary("data/dico.txt");
+        Bag bag;
+        Player player(bag);
+        Board board(bag, dictionary);
 
-    for(unsigned int i = 0 ; i < 20 ; ++i) { player.playBestMove(board, false); }
+        for(unsigned int i = 0 ; i < 20 ; ++i) { player.playBestMove(board, false); }
 
-    std::cout << '\n';
+        std::cout << '\n';
 
-    if(!board.testAllWordsOnBoard()) { return -1; }
-    std::cout << "No incorrect words were found on the board.\n";
+        if(!board.testAllWordsOnBoard()) {
+            board.saveToFile("data/test_boards_wrong_board.txt");
+            return -1;
+        }
+
+        std::cout << "No incorrect words were found on the board.\n";
+    }
 
     return 0;
 }
